3c46235cfc5ddc9295abda0d4caba9e5023656a8
Ben <baguirre@luc.edu>
2023-12-07 10:49:09 -0600

    message schema + backlinks

--- models/message.js
+++ models/message.js
@@ -1,10 +1,10 @@
-// const mongoose = require('mongoose')
-// const Schema = mongoose.Schema
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
-// const messageSchema = new Schema({
-//     user: '',
-//     time: '',
-//     text: '',
-// })
+const messageSchema = new Schema({
+    artist: { type: Schema.Types.ObjectId, ref: "User" },
+    time: { type: Date },
+    text: { type: String, required: true },
+})
 
-// module.exports = mongoose.model('message', messageSchema)
+module.exports = mongoose.model('message', messageSchema)
