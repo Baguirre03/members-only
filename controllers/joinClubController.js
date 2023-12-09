f025741dedbab0cbb93d995cecad67e77dd3e56a
Ben <baguirre@luc.edu>
2023-12-09 14:19:01 -0600

    update club

--- controllers/joinClubController.js
+++ controllers/joinClubController.js
@@ -1,5 +1,9 @@
 const asyncHandler = require('express-async-handler')
 const authenticate = require('../config/passport.js')
+const { body, validationResult } = require('express-validator')
+const User = require('../models/user.js')
+const Message = require('../models/message.js')
+const login = require('../config/passport.js')
 
 
 exports.get_joinclub = asyncHandler(async (req, res, next) => {
@@ -8,3 +12,37 @@ exports.get_joinclub = asyncHandler(async (req, res, next) => {
         user: req.user,
     })
 })
+
+exports.post_joinclub = [
+    body('secret_password')
+        .trim()
+        .isLength({ min: 1 })
+        .escape()
+        .matches('TOP'),
+
+    asyncHandler(async (req, res, next) => {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            res.render('joinclub', {
+                title: "Join the club TODAY!",
+                user: req.user,
+                errors: errors.array()
+            })
+        } else {
+            await User.updateOne({ _id: req.user._id }, {
+                $set: {
+                    membership: true
+                }
+            });
+            const user = await User.find({ _id: req.user._id })
+            console.log(user)
+            res.render('messages', {
+                title: "Messages",
+                user: req.user,
+                messages: await Message.find({}).exec()
+            })
+        }
+
+    })
+]
--- routes/joinclub.js
+++ routes/joinclub.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const joinclubController = require('../controllers/joinClubController.js')
 
 router.get('/', joinclubController.get_joinclub)
+router.post('/', joinclubController.post_joinclub)
 
 
 module.exports = router
