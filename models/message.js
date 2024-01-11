a87c9621f24687b3b38094d64855ab96427e99d2
Ben <baguirre@luc.edu>
2024-01-11 13:24:11 -0500

    format message

--- models/message.js
+++ models/message.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+const { DateTime } = require('luxon')
 
 const messageSchema = new Schema({
     user: { type: Schema.Types.ObjectId, ref: "user" },
     time: { type: Date },
     text: { type: String, required: true },
+})
+
+messageSchema.virtual('date_formatted').get(function () {
+    return DateTime.fromJSDate(this.time).toLocaleString(DateTime.DATETIME_MED);
 })
 
 module.exports = mongoose.model('message', messageSchema)
